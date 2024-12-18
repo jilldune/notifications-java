aeb831380ce8a87126aac847ce46ba9346be07e4
jilldune <dunejill@gmail.com>
2024-12-18 00:56:22 +0000

    Changed the options implementation changed implementation of Notifications code implementation from Map.of() to HashMap<>() for consistency

--- src/main/java/com/edubiz/notificationsjava/Entry.java
+++ src/main/java/com/edubiz/notificationsjava/Entry.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -16,24 +17,24 @@ public class Entry extends Application {
 //        toast.toast("This is a toast, thing in the name of God ooo heyy heyy ehyy",0);
 
 //        Notification
-        Notification notif = new Notification();
-        Map<String, Object> options = Map.of(
-                "header","My new notification",
-                "body","This is a test notification message. Thank you",
-                "duration",0,
-                "autoClose",false,
-                "buttons",new LinkedHashMap<String,Map<String,Object>>(){{
-                    put("cancel",Map.of(
-                            "action",(Runnable)()-> System.out.println("action for button")
+        Notification notify = new Notification();
+        //                            "style","-fx-background-color: blue;-fx-text-fill: white;"
+        Map<String, Object> options = new HashMap<>();
+        options.put("header", "My new notification");
+        options.put("body", "This is a test notification message. Thank you");
+        options.put("duration", 0);
+        options.put("autoClose", false);
+        options.put("buttons", new LinkedHashMap<String, Map<String, Object>>() {{
+            put("cancel", Map.of(
+                    "action", (Runnable) () -> System.out.println("action for button")
 //                            "style","-fx-background-color: blue;-fx-text-fill: white;"
-                    ));
-                    put("delete",Map.of(
-                            "action",(Runnable)()-> System.out.println("action for delete"),
-                            "style","-fx-background-color: red;-fx-text-fill: white;"
-                    ));
-                }}
-        );
-        notif.notification(options);
+            ));
+            put("delete", Map.of(
+                    "action", (Runnable) () -> System.out.println("action for delete"),
+                    "style", "-fx-background-color: red;-fx-text-fill: white;"
+            ));
+        }});
+        notify.notification(options);
 
 //        Prompt prompt = new Prompt("Enter our name","name");
 //        prompt.show();
