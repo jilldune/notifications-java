c95b3f8b4be3ae30c8e1d5b48a352c09ea90502a
jilldune <dunejill@gmail.com>
2024-12-21 20:06:30 +0000

    Added type selection class

---
+++ src/main/java/com/edubiz/notificationsjava/Notify/NotifierInputType.java
@@ -0,0 +1,21 @@
+package com.edubiz.notificationsjava.Notify;
+
+public enum NotifierInputType {
+    TEXT("text"),
+    TEXTAREA("textarea");
+
+    private final String value;
+
+    NotifierInputType(String value) { this.value = value; }
+
+    public String getValue() { return this.value; }
+
+    public static NotifierInputType fromString(String value) {
+        for (NotifierInputType input : NotifierInputType.values()) {
+            if (input.value.equalsIgnoreCase(value))
+                return input;
+        }
+
+        throw new IllegalArgumentException("Invalid notification position:"+value);
+    }
+}
