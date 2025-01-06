9d356357ea2127b7143bbc1fb75f235977ccfc7a
jilldune <dunejill@gmail.com>
2025-01-06 18:25:51 +0000

    New PropertiesWrapper class - Not used

---
+++ src/main/java/com/edubiz/notificationsjava/NotifierUtil/PropertiesWrapper.java
@@ -0,0 +1,17 @@
+package com.edubiz.notificationsjava.NotifierUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PropertiesWrapper {
+    private final Map<String, Object> properties = new HashMap<>();
+
+    public <T> void put(String key, T value) {
+        properties.put(key, value);
+    }
+
+    public <T> T get(String key, Class<T> clazz) {
+        return clazz.cast(properties.get(key));
+    }
+}
+
