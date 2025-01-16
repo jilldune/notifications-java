6b9662f109e8b1aecca53424225a32f8910e96e7
jilldune <dunejill@gmail.com>
2025-01-16 18:13:49 +0000

    General modifications

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,14 +8,12 @@ module com.edubiz.notificationsjava {
     requires annotations;
     requires java.desktop;
 
-    exports com.edubiz.notificationsjava.Notify;
-    opens com.edubiz.notificationsjava.Notify to javafx.fxml;
-
-    // comment when building
-    exports com.edubiz.notificationsjava.test;
-    opens com.edubiz.notificationsjava.test to javafx.fxml;
     exports com.edubiz.notificationsjava.Managers;
     opens com.edubiz.notificationsjava.Managers to javafx.fxml;
     exports com.edubiz.notificationsjava.NotifierUtil;
     opens com.edubiz.notificationsjava.NotifierUtil to javafx.fxml;
+    exports com.edubiz.notificationsjava.Notify;
+    opens com.edubiz.notificationsjava.Notify to javafx.fxml;
+
+    exports com.edubiz.notificationsjava.test;
 }
