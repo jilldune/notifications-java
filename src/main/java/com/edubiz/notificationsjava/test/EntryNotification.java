e119474c10fefb6d1db5a72e1a1fb5a56dc5c343
Mensah Benjamin <dunejill@gmail.com>
2025-05-19 12:34:12 +0000

    Code modification in EntryNotification

--- src/main/java/com/edubiz/notificationsjava/test/EntryNotification.java
+++ src/main/java/com/edubiz/notificationsjava/test/EntryNotification.java
@@ -7,17 +7,20 @@ import com.edubiz.notificationsjava.NotifierUtil.NotifyInput;
 import com.edubiz.notificationsjava.NotifierUtil.NotifyType;
 import com.edubiz.notificationsjava.Notify.*;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
 
 public class EntryNotification extends Application {
     @Override
     public void start(Stage stage) {
         // create  the root
-        AnchorPane root = new AnchorPane();
+        StackPane root = new StackPane();
         root.setPrefSize(800,600);
         root.setStyle("-fx-background-color: red;");
 
@@ -25,49 +28,25 @@ public class EntryNotification extends Application {
 
         stage.setOnCloseRequest(ev -> manager.shutDown());
 
-        Button button = new Button("See MyNotifier");
-        button.setCursor(Cursor.HAND);
-        button.setOnAction(e -> {
-            // Prompt
-            Prompt prompt = manager.create(NotifyType.PROMPT);
-            prompt.setHeader("Prompt")
-                    .setBody("Hi there this is a prompt \nclick my buttons to view other components")
-                    .autoClose(false)
-                    .setAnimation(true)
-                    .setButton("dialog",()->{
-                        Dialog dialog = manager.create(NotifyType.DIALOG);
-                        dialog.setOnCloseRequest(()-> System.out.println("On close request..."));
-                        dialog.setOnClosed(()-> System.out.println("on close..."));
-                        dialog.setHeader("Dialog Notification")
-                                .setButton("save",(text) -> System.out.println(text))
-                                .setButton("cancel",() -> {})
-                                .bindInputAction(0)
-                                .setLabel("This is a dialogue body\nTell us about you.")
-                                .setType(NotifyInput.TEXTAREA)
-                                .create();
-                    })
-                    .setButton("notification",()->{
-                        Notification notify = manager.create(NotifyType.NOTIFICATION);
-                        notify.setHeader("Notification")
-                                .setBody("I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation")
-                                .setPosition(NotifyPos.TOP)
-                                .setAnimation(true)
-                                .setAlertType(NotifyAlert.ALERT)
-                                .setDuration(5.5)
-                                .setButton("exit", ()-> System.exit(0),"-fx-background-color: red;-fx-border-color: red;-fx-text-fill: white;")
-                                .setButton("cancel",()->{})
-                                .setButton("cancel me",()->{})
-                                .create();
-                    })
-                    .setButton("toast",()->{
-                        Toast  toastNotifier = manager.create(NotifyType.TOAST);
-                        toastNotifier.create(NotifyAlert.SUCCESS,"I am a toast");
-                    })
-                    .create();
-
+        Button notification = getNotification(manager);
+        // DIALOG
+        Button dialog = getDialog(manager);
+        // TOAST
+        Button toast = new Button("Toast");
+        toast.setCursor(Cursor.HAND);
+        toast.setOnAction(e -> {
+            Toast  toastNotifier = manager.create(NotifyType.TOAST);
+            toastNotifier.create(NotifyAlert.SUCCESS,"I am a toast",0.0);
         });
+        // PROMPT
+        Button prompt = getPrompt(manager);
 
-        root.getChildren().add(button);
+        VBox container = new VBox(10);
+        container.getChildren().addAll(notification,prompt,toast,dialog);
+        container.setAlignment(Pos.TOP_CENTER);
+        container.setFillWidth(true);
+
+        root.getChildren().add(container);
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -76,6 +55,61 @@ public class EntryNotification extends Application {
 
         // Show the stage
         stage.show();
+    }
+
+    @NotNull
+    private static Button getPrompt(NotifyManager manager) {
+        Button prompt = new Button("Prompt");
+        prompt.setCursor(Cursor.HAND);
+        prompt.setOnAction(e -> {
+            // Prompt
+            Prompt promptNotify = manager.create(NotifyType.PROMPT);
+            promptNotify.setHeader("Prompt")
+                    .setBody("Hi there this is a prompt \nclick my buttons to view other components")
+                    .autoClose(false)
+                    .setAnimation(true)
+                    .setButton("cancel",()->{})
+                    .create();
+        });
+        return prompt;
+    }
+
+    @NotNull
+    private static Button getDialog(NotifyManager manager) {
+        Button dialog = new Button("Dialog");
+        dialog.setCursor(Cursor.HAND);
+        dialog.setOnAction(e -> {
+            Dialog dialogNotification = manager.create(NotifyType.DIALOG);
+            dialogNotification.setOnCloseRequest(()-> System.out.println("On close request..."));
+            dialogNotification.setOnClosed(()-> System.out.println("on close..."));
+            dialogNotification.setHeader("Dialog Notification")
+                    .setButton("save",(text) -> System.out.println(text))
+                    .setButton("cancel",() -> {})
+                    .bindInputAction(0)
+                    .setLabel("This is a dialogue body\nTell us about you.")
+                    .setType(NotifyInput.TEXTAREA)
+                    .create();
+        });
+        return dialog;
+    }
+
+    @NotNull
+    private static Button getNotification(NotifyManager manager) {
+        Button notification = new Button("Notification");
+        notification.setCursor(Cursor.HAND);
+        notification.setOnAction(e -> {
+            Notification notify = manager.create(NotifyType.NOTIFICATION);
+            notify.setHeader("Notification")
+                    .setBody("I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation I am a notification for the next generation")
+                    .setPosition(NotifyPos.TOP)
+                    .setAnimation(true)
+                    .setAlertType(NotifyAlert.ALERT)
+                    .setButton("exit", ()-> System.exit(0),"-fx-background-color: red;-fx-border-color: red;-fx-text-fill: white;")
+                    .setButton("cancel",()->{})
+                    .setButton("cancel me",()->{})
+                    .create();
+        });
+        return notification;
     }
 
     public static void main(String[] args) {
